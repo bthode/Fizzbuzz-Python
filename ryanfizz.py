feb2e3744952f005b9f7ab48d059fd40971455e1
rsweet2 <rsweet2@users.noreply.github.com>
2022-07-07 12:19:20 -0700

    changing text to fizz buzz.

--- ryanfizz.py
+++ ryanfizz.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
-        print ( str(i) + ' is Multiple of 3 AND 5')
+        print ('FizzBuzz')
     elif i % 3 == 0:
-        print ( str(i) + ' is a multiple of 3')
+        print ('Fizz')
     elif i % 5 == 0:
-        print ( str(i) + ' is a multiple of 5')
+        print ('Buzz')
     else:
-        print ( str(i) )
+        print (str(i))
